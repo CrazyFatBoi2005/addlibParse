c3ab8614e272b73a7716d6b976d14456eb004ab2
crazysadboy <spa.ez@mail.ru>
2023-10-08 17:41:55 +0300

    multiprocess works!

--- parse_api/parsing_api.py
+++ parse_api/parsing_api.py
@@ -1,4 +1,5 @@
 import requests
+import multiprocessing as mp
 from flask import Flask, render_template, redirect, request, jsonify
 
 from parse_api.db_data import db_session
@@ -17,9 +18,12 @@ def add_new_account(id):
     url = f"https://www.facebook.com/ads/library/?active_status=all" \
           f"&ad_type=all&country=ALL&view_all_page_id={id}" \
           f"&sort_data[direction]=desc&sort_data[mode]=relevancy_monthly_grouped&search_type=page&media_type=all "
-    requests.post("http://127.0.0.1:5000/ok_status")
-    parse_page(url, {})
-    requests.post("http://127.0.0.1:5000/")
+
+    process = mp.Process(target=parse_page, args=(url, {}))
+    process.start()
+    response = jsonify({"message": "OK"})
+    response.status_code = 200
+    return response
 
 
 def main():
